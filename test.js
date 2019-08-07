ba1ec705a55937c8c8e9d0406e633d2a7b4b2def
sarini68 <34100016+sarini68@users.noreply.github.com>
2019-08-07 12:40:15 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,34 @@
+
+class NitroBlockAAA{
+    getInfo() {
+        return {
+            "id": "NitroBlockAAA",
+            "name": "NitroBlockAAA",
+            "blocks": [{
+                    "opcode": "substringy",
+                    "blockType": "reporter",
+                    "text": "letters [num1] through [num2] of [string]",
+                    "arguments": {
+                        "num1": {
+                            "type": "number",
+                            "defaultValue": "2"
+                        },
+                        "num2": {
+                            "type": "number",
+                            "defaultValue": "5"
+                        },
+                        "string": {
+                            "type": "string",
+                            "defaultValue": "hello world"
+                        }
+                    }
+            }],
+        "menus": { //we will get back to this in a later tutorial
+        }
+    };
+    }
+    substringy({num1, num2, string}) {
+        return string.substring(num1 - 1, num2);
+    };
+}
+Scratch.extensions.register(new NitroBlockAAA());
